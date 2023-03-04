7222c539cce2e3df80271da6a3d9aabe2be62c22
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-04 00:50:32 -0700

    all of front end completed with styiling.

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className="message owner" >
+      <div className="messageInfo">
+        <img src="https://media.licdn.com/dms/image/D4E03AQHKImtA61x27Q/profile-displayphoto-shrink_200_200/0/1675379924276?e=1683158400&v=beta&t=2O2zncaRCXoA-YBypH2O8pFR2DbptByMkC47rZoDwYQ" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+
+      <p>hello</p>
+      <img src="https://media.licdn.com/dms/image/D4E03AQHKImtA61x27Q/profile-displayphoto-shrink_200_200/0/1675379924276?e=1683158400&v=beta&t=2O2zncaRCXoA-YBypH2O8pFR2DbptByMkC47rZoDwYQ" alt="" />
+      </div>
+    </div>
   )
 }
 
